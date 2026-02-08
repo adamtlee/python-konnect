d5b3f84ac4fda6a13536ac7b26f5472fe1be9f87
adamtlee <adam.lee346@gmail.com>
2026-02-07 17:22:42 -0700

    refactored athlete model

--- athletes/admin.py
+++ athletes/admin.py
@@ -4,7 +4,22 @@ from .models import Athlete
 
 @admin.register(Athlete)
 class AthleteAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'organization', 'created_at')
-    list_filter = ('organization', 'created_at')
-    search_fields = ('first_name', 'last_name', 'email', 'organization')
+    list_display = ('first_name', 'last_name', 'nickname', 'organization', 'weight', 'height', 'age', 'country', 'team', 'created_at')
+    list_filter = ('organization', 'country', 'team', 'created_at')
+    search_fields = ('first_name', 'last_name', 'nickname', 'organization', 'country', 'team')
     readonly_fields = ('created_at',)
+    
+    fieldsets = (
+        ('Personal Information', {
+            'fields': ('first_name', 'last_name', 'nickname', 'age', 'country')
+        }),
+        ('Physical Attributes', {
+            'fields': ('weight', 'height')
+        }),
+        ('Organization', {
+            'fields': ('organization', 'team')
+        }),
+        ('Metadata', {
+            'fields': ('created_at',)
+        }),
+    )
--- athletes/models.py
+++ athletes/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 
 class Athlete(models.Model):
+    organization = models.CharField(max_length=200)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    organization = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=100, blank=True, null=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, help_text="Weight in pounds (lbs)")
+    height = models.CharField(max_length=20, blank=True, null=True, help_text="Height (e.g., 5'10\" or 178cm)")
+    age = models.IntegerField(blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    team = models.CharField(max_length=200, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -14,4 +19,6 @@ class Athlete(models.Model):
         verbose_name_plural = 'Athletes'
 
     def __str__(self):
+        if self.nickname:
+            return f"{self.first_name} '{self.nickname}' {self.last_name}"
         return f"{self.first_name} {self.last_name}"
